98f29511f61d433a6923bf0bac063c39c918ed5d
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-05-15 15:41:06 +0100

    What you looking at

---
+++ tools/findlinks.pl
@@ -0,0 +1,27 @@
+use strict;
+use warnings;
+use File::Spec;
+use autodie;
+use CPAN::DistnameInfo;
+
+my $dir = shift or die;
+die unless -d $dir;
+
+my %data;
+
+opendir my $DIR, $dir;
+
+while( my $file = readdir $DIR ) {
+  next if $file =~ /^\./ or $file !~ /^perl.+(bz2|gz)$/;
+  my $abs = File::Spec->catfile( $dir, $file );
+  next unless -l $abs;
+  my $link = readlink $abs;
+  $link =~ s#^.*/authors/id/##;
+  my $d = CPAN::DistnameInfo->new( $link );
+  $data{ $d->version }->{ $d->extension} = $link;
+}
+
+{
+  use Data::Dumper::Concise;
+  print Dumper( \%data );
+}
