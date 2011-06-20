523a5971bec0b93cb7450850d86664a599a4fb90
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2011-06-21 00:43:11 +0100

    Updated for v5.15.0

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -199,6 +199,10 @@ our $data =
     "tar.bz2" => "J/JE/JESSE/perl-5.14.1.tar.bz2",
     "tar.gz" => "J/JE/JESSE/perl-5.14.1.tar.gz",
   },
+  "5.15.0" => {
+    "tar.bz2" => "D/DA/DAGOLDEN/perl-5.15.0.tar.bz2",
+    "tar.gz" => "D/DA/DAGOLDEN/perl-5.15.0.tar.gz",
+  },
   "5.6.0" => {
     "tar.gz" => "G/GS/GSAR/perl-5.6.0.tar.gz",
   },
