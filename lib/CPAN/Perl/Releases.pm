1fb816435bf08c96bd7f2059a73d1ee807d184c4
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-05-16 07:42:28 +0100

    Updated for v5.16.0-RC2

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -257,6 +257,10 @@ our $data =
     "tar.bz2" => "R/RJ/RJBS/perl-5.16.0-RC1.tar.bz2",
     "tar.gz" => "R/RJ/RJBS/perl-5.16.0-RC1.tar.gz",
   },
+  "5.16.0-RC2" => {
+    "tar.bz2" => "R/RJ/RJBS/perl-5.16.0-RC2.tar.bz2",
+    "tar.gz" => "R/RJ/RJBS/perl-5.16.0-RC2.tar.gz",
+  },
   "5.6.0" => {
     "tar.gz" => "G/GS/GSAR/perl-5.6.0.tar.gz",
   },
