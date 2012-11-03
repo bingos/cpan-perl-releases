7caec9a17b31ff90fe95be68ccc66171d3442979
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-11-03 21:38:52 +0000

    Updated for v5.12.5-RC1

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -129,6 +129,10 @@ our $data =
     "tar.bz2" => "L/LB/LBROCARD/perl-5.12.4.tar.bz2",
     "tar.gz" => "L/LB/LBROCARD/perl-5.12.4.tar.gz",
   },
+  "5.12.5-RC1" => {
+    "tar.bz2" => "D/DO/DOM/perl-5.12.5-RC1.tar.bz2",
+    "tar.gz" => "D/DO/DOM/perl-5.12.5-RC1.tar.gz",
+  },
   "5.13.0" => {
     "tar.bz2" => "L/LB/LBROCARD/perl-5.13.0.tar.bz2",
     "tar.gz" => "L/LB/LBROCARD/perl-5.13.0.tar.gz",
