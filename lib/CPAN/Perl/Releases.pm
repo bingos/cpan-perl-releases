dfd81b85147e6bf85fda0ef855c542ca6c242883
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-11-20 23:56:50 +0000

    Updated for v5.17.6

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -328,6 +328,10 @@ our $data =
     "tar.bz2" => "F/FL/FLORA/perl-5.17.5.tar.bz2",
     "tar.gz" => "F/FL/FLORA/perl-5.17.5.tar.gz",
   },
+  "5.17.6" => {
+    "tar.bz2" => "R/RJ/RJBS/perl-5.17.6.tar.bz2",
+    "tar.gz" => "R/RJ/RJBS/perl-5.17.6.tar.gz",
+  },
   "5.6.0" => {
     "tar.gz" => "G/GS/GSAR/perl-5.6.0.tar.gz",
   },
