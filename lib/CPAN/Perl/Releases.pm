d369eb6b8a337a0365e74f79af7c1b7cca5a0f3e
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-04-21 09:23:41 +0100

    Updated for v5.17.11

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -368,6 +368,10 @@ our $data =
     "tar.bz2" => "C/CO/CORION/perl-5.17.10.tar.bz2",
     "tar.gz" => "C/CO/CORION/perl-5.17.10.tar.gz",
   },
+  "5.17.11" => {
+    "tar.bz2" => "R/RJ/RJBS/perl-5.17.11.tar.bz2",
+    "tar.gz" => "R/RJ/RJBS/perl-5.17.11.tar.gz",
+  },
   "5.6.0" => {
     "tar.gz" => "G/GS/GSAR/perl-5.6.0.tar.gz",
   },
