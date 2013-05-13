3882ae87524c944ef1c63ead804c583e9165a43b
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-05-13 22:59:54 +0100

    Updated for v5.18.0-RC3

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -380,6 +380,10 @@ our $data =
     "tar.bz2" => "R/RJ/RJBS/perl-5.18.0-RC2.tar.bz2",
     "tar.gz" => "R/RJ/RJBS/perl-5.18.0-RC2.tar.gz",
   },
+  "5.18.0-RC3" => {
+    "tar.bz2" => "R/RJ/RJBS/perl-5.18.0-RC3.tar.bz2",
+    "tar.gz" => "R/RJ/RJBS/perl-5.18.0-RC3.tar.gz",
+  },
   "5.6.0" => {
     "tar.gz" => "G/GS/GSAR/perl-5.6.0.tar.gz",
   },
