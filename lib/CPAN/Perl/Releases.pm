1976ed8383af41db76da989b9f2b288d57e7b3af
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2015-01-20 20:31:05 +0000

    Updated for v5.21.8

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -157,6 +157,7 @@ our $data =
 "5.21.5" => { id => 'ABIGAIL' },
 "5.21.6" => { id => 'BINGOS', xz => 1 },
 "5.21.7" => { id => 'CORION', xz => 1 },
+"5.21.8" => { id => 'WOLFSAGE', xz => 1 },
 };
 
 sub perl_tarballs {
