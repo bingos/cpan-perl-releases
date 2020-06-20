593b019b095713e9a7ec42057b9d8cbf5757f8f2
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2020-06-20 20:35:07 +0100

    Updated for v5.32.0

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -262,6 +262,7 @@ our $data =
 "5.30.3-RC1" => { id => 'XSAWYERX' },
 "5.30.3" => { id => 'XSAWYERX' },
 "5.32.0-RC1" => { id => 'XSAWYERX' },
+"5.32.0" => { id => 'XSAWYERX' },
 };
 
 sub perl_tarballs {
