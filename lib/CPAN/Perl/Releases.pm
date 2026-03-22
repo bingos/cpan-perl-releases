25cbaf32ab69f9b24ad1ec0a1d47600c00d3b2eb
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2026-03-22 21:27:29 +0000

    Updated for RC1s of v5.40.4 and v5.42.2

--- lib/CPAN/Perl/Releases.pm
+++ lib/CPAN/Perl/Releases.pm
@@ -347,6 +347,8 @@ our $data =
 "5.42.1-RC1" => { id => 'SHAY' },
 "5.42.1" => { id => 'SHAY' },
 "5.43.9" => { id => 'EHERMAN' },
+"5.40.4-RC1" => { id => 'SHAY' },
+"5.42.2-RC1" => { id => 'SHAY' },
 };
 
 sub perl_tarballs {
